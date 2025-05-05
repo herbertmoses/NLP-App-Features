21fc5bcbe5b25bcaea6742eb0affd733ea1ead60
HERBERT MOSES <110626716+herbertmoses@users.noreply.github.com>
2025-05-05 20:20:23 +0000

    enhanced UI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+import ast  # To safely parse string representations of tuples/lists
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -9,16 +10,24 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+    # Convert form inputs to float
     float_features = [float(x) for x in request.form.values()]
     final_features = [np.array(float_features)]
+
+    # Get prediction from model
     prediction = model.predict(final_features)
 
-    return render_template('index.html', prediction_text='Top Features are {}'.format(prediction))
+    # Parse the stringified list of tuples (e.g., from NumPy array)
+    prediction_list_str = prediction[0]  # First string in array
+    try:
+        parsed_tuples = ast.literal_eval(prediction_list_str)
+    except Exception as e:
+        parsed_tuples = [("Error parsing prediction", str(e))]
+
+    # Pass parsed data to the template
+    return render_template('index.html', prediction_text=parsed_tuples)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
